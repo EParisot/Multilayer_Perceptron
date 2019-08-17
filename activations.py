fixed softmax

activations.py
@@ -13,7 +13,10 @@ def tanh(x):
     return np.tanh(x)
 
 def softmax(x):
-  return np.exp(x) / np.sum(np.exp(x))
+  ret = []
+  for elem in x:
+    ret.append(np.exp(elem) / np.sum(np.exp(elem)))
+  return np.array(ret)
 
 activations_dict = {
   "sigmoid": sigmoid,

model.py
@@ -17,7 +17,7 @@ class Model(object):
         for epoch in range(epochs):
             for batch_idx in range(0, X.shape[1], batch_size):
                 batch = X[:, batch_idx : batch_idx + batch_size]
-                labels_batch = Y[batch_idx : batch_idx + batch_size]
+                batch_labels = Y[batch_idx : batch_idx + batch_size]
                 # batch loop
                 for i, layer in enumerate(self.layers):
                     # feedforward
@@ -27,15 +27,15 @@ class Model(object):
                         layer.feedforward(self.layers[i - 1].layer_out)
                 
                 # calc step error
-                loss_1 = np.multiply(labels_batch, np.log(self.layers[-1].layer_out))
-                loss_2 = np.multiply((1 - labels_batch), np.log(1 - self.layers[-1].layer_out))
+                loss_1 = np.multiply(batch_labels, np.log(layer.layer_out))
+                loss_2 = np.multiply((1 - batch_labels), np.log(1 - layer.layer_out))
                 batch_loss = -np.mean(loss_1 + loss_2)
-                print(batch_loss, labels_batch, layer.layer_out)
+                print(batch_loss, batch_labels, layer.layer_out)
                                     
                 # backprop
                 for layer in reversed(range(len(self.layers))):
                     if not isinstance(self.layers[layer], Input):
                         if self.layers[layer].is_last:
-                            self.layers[layer].backprop(labels_batch)
+                            self.layers[layer].backprop(batch_labels)
                         else:
                             self.layers[layer].backprop(self.layers[layer + 1])
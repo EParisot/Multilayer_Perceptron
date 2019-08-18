2040165469455cb82a892c46387e112bff714fa1
eparisot <eparisot@student.42.fr>
2019-08-18 18:09:48 +0200

    prints

--- layers.py
+++ layers.py
@@ -7,26 +7,24 @@ class Input(object):
         self.layer_out = np.zeros(self.width)
     
     def show(self):
-        print("Input             : %s features" % (self.width))
+        print("Input             : %s features \n" % (self.width))
     
 class FC(object):
     def __init__(self, in_shape, width, activation, is_last=False):
-        self.act_name = activation
-        self.activation = activations_dict[activation]
-        if self.act_name != "softmax":
-            self.deriv_act = derivatives_dict[activation]
+        self.in_shape = in_shape
         self.is_last = is_last
         if is_last == False:
             self.width = width + 1
         else:
             self.width = width
-        self.in_shape = in_shape
+        self.act_name = activation
+        self.activation = activations_dict[activation]
+        if self.act_name != "softmax":
+            self.deriv_act = derivatives_dict[activation]
         self.weights = np.random.random_sample((self.width, self.in_shape))
         self.z = np.zeros((self.width,))
         self.layer_out = np.zeros((self.width,))
     
     def show(self):
         print("FullyConnected    : %s perceptrons, activation : %s" % (self.width, self.act_name))
-        print(self.in_shape)
-        print(self.weights.shape)
-        print(self.layer_out.shape)
+        print("in_shape : %s, weights : %s, out_shape : %s \n" % (self.in_shape, self.weights.shape, self.layer_out.shape[0]))

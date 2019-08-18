676ae1eafee1619008c43548d65c0c36241e482e
eparisot <eparisot@student.42.fr>
2019-08-18 18:03:20 +0200

    batch

--- model.py
+++ model.py
@@ -18,29 +18,33 @@ class Model(object):
             for batch_idx in range(0, X.shape[1], batch_size):
                 batch = X[:, batch_idx : batch_idx + batch_size]
                 batch_labels = Y[batch_idx : batch_idx + batch_size]
+
+                for layer in self.layers:
+                    layer.gradients = []
+                batch_loss = []
+
                 # loop over batch
-                for i, col in enumerate(batch.T):
+                for i, row in enumerate(batch.T): # TO REMOVE
                     # feedforward
                     for j, layer in enumerate(self.layers):
                         if isinstance(layer, Input):
                             layer.layer_out = np.zeros(layer.width)
                             layer.layer_out[0] = 1.
-                            for k, val in enumerate(col):
+                            for k, val in enumerate(row):
                                 layer.layer_out[k+1] = val
                         else:
                             layer.layer_in = self.layers[j - 1].layer_out
                             if layer.is_last == False:
                                 layer.layer_in[0] = 1.
-                            print(layer.layer_in)
                             layer.z = np.dot(layer.layer_in, layer.weights.T)
                             layer.layer_out = layer.activation(layer.z)
-                            print(layer.layer_out)
 
                     # calc step error
                     loss_1 = np.multiply(batch_labels[i], np.log(layer.layer_out))
                     loss_2 = np.multiply((1 - batch_labels[i]), np.log(1 - layer.layer_out))
                     step_loss = -np.mean(loss_1 + loss_2)
-                    print(step_loss, batch_labels[i], layer.layer_out)
+                    batch_loss.append(step_loss)
+                    #print(step_loss, batch_labels[i], layer.layer_out)
 
                     # backprop
                     for layer in reversed(range(len(self.layers))):
@@ -55,9 +59,12 @@ class Model(object):
                     # compute gradients
                     for j, layer in enumerate(self.layers):
                         if not isinstance(layer, Input):
-                            layer.gradients = np.outer(layer.deltas, self.layers[j-1].layer_out)
+                            layer.gradients.append(np.outer(layer.deltas, self.layers[j-1].layer_out))
 
-                    # update weights
-                    for j, layer in enumerate(self.layers):
-                        if not isinstance(layer, Input):
-                            layer.weights -= lr * layer.gradients
+                # update weights
+                for j, layer in enumerate(self.layers):
+                    if not isinstance(layer, Input):
+                        layer.weights -= lr * np.mean(layer.gradients, axis=0)
+                    
+                batch_los = np.mean(batch_loss)
+                print(batch_los)
